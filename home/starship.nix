faeb2b07926a9e09a6925c485282518d79caa5d6
gaurav2361 <gaurav23617@gmail.com>
2026-03-05 09:54:37 +0530

    feat: starship enable disable added
    Signed-off-by: gaurav2361 <gaurav23617@gmail.com>

--- home/starship.nix
+++ home/starship.nix
@@ -4,9 +4,18 @@
   pkgs,
   ...
 }:
+let
+  cfg = config.terminal.starship;
+in
 {
-  programs.starship = {
-    enable = true;
-    settings = pkgs.lib.importTOML ../config/starship.toml;
+  options.terminal.starship = {
+    enable = lib.mkEnableOption "starship with custom dotfiles symlink";
+  };
+  config = lib.mkIf cfg.enable {
+
+    programs.starship = {
+      enable = true;
+      settings = pkgs.lib.importTOML ../config/starship.toml;
+    };
   };
 }
