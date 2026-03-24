79ce7ed24541478d817a257ae71925efbd4c9d27
gaurav2361 <gaurav23617@gmail.com>
2026-03-24 10:01:48 +0530

    refactor(lib): flatten overlays and standardOverlays

--- lib/default.nix
+++ lib/default.nix
@@ -4,21 +4,16 @@ let
   configHelper = import ./configHelper.nix { inherit inputs; };
   moduleHelper = import ./moduleHelper.nix { inherit lib; };
 in
-lib
-// {
+lib // {
   # Merge our system strings into the standard lib.systems
   systems = lib.systems // configHelper.systems;
-
-  # Flatten common helpers
-  inherit (configHelper) forAllSystems;
-
-  # Internal Flake Helpers
-  flake = {
-    inherit (configHelper)
-      standardOverlays
-      overlays
-      ;
-  };
+  
+  # Flatten common helpers and metadata
+  inherit (configHelper) 
+    forAllSystems 
+    standardOverlays 
+    overlays
+    ;
 
   # Flattened Host Creation Helpers
   inherit (configHelper)
