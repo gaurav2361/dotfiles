93fb68ae1a1bb1343b9f19db933a41464788d423
gaurav2361 <gaurav23617@gmail.com>
2026-04-26 10:53:29 +0530

    fix(darwin): disable direnv checkPhase to fix build hang

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,6 +12,12 @@
       config.allowUnfree = true;
     };
 
+    # Fix direnv hanging on aarch64-darwin during checkPhase
+    # https://github.com/NixOS/nixpkgs/issues/507531
+    direnv = prev.direnv.overrideAttrs (oldAttrs: {
+      doCheck = if final.stdenv.isDarwin then false else (oldAttrs.doCheck or true);
+    });
+
     # Fix Jeepney D-Bus test failures and import checks
     # pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     #   (python-final: python-prev: {
