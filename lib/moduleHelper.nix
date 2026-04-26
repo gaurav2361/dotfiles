f8146318e5b24d93ddcd8f9f287590baee66c64b
gaurav2361 <gaurav23617@gmail.com>
2026-04-26 23:48:16 +0530

    refactor(lib): revert mkModule helper to original state

--- lib/moduleHelper.nix
+++ lib/moduleHelper.nix
@@ -5,19 +5,16 @@
     {
       name,
       description ? "Enable ${name} module",
-      options ? { },
       config,
       globalConfig,
-      declareEnable ? true,
     }:
     {
-      options = lib.setAttrByPath (lib.splitString "." "modules.${name}") (
-        lib.optionalAttrs declareEnable {
-          enable = lib.mkEnableOption description;
-        }
-        // options
-      );
-      config = lib.mkIf (lib.attrByPath (lib.splitString "." "modules.${name}.enable") false globalConfig) config;
+      options = lib.setAttrByPath (lib.splitString "." "modules.${name}") {
+        enable = lib.mkEnableOption description;
+      };
+      config = lib.mkIf (lib.attrByPath (lib.splitString "." "modules.${name}.enable") false
+        globalConfig
+      ) config;
     };
 
   # Helper to create a simple module with an enable option for Home Manager (no predefined prefix)
@@ -25,24 +22,21 @@
     {
       name,
       description ? "Enable ${name} module",
-      options ? { },
       config,
       globalConfig,
       enableDefault ? false,
-      declareEnable ? true,
     }:
     {
-      options = lib.setAttrByPath (lib.splitString "." name) (
-        lib.optionalAttrs declareEnable {
-          enable = lib.mkOption {
-            type = lib.types.bool;
-            default = enableDefault;
-            description = description;
-          };
-        }
-        // options
-      );
-      config = lib.mkIf (lib.attrByPath (lib.splitString "." "${name}.enable") enableDefault globalConfig) config;
+      options = lib.setAttrByPath (lib.splitString "." name) {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = enableDefault;
+          description = description;
+        };
+      };
+      config = lib.mkIf (lib.attrByPath (lib.splitString "." "${name}.enable") enableDefault
+        globalConfig
+      ) config;
     };
 
   # Helper for boolean options
